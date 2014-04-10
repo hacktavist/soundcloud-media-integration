788d57068df7de2eb66405bb66db4bc2bffd1864
Brandon <brandon@gooddonegreat.com>
2014-04-10 14:08:13 -0400

    Auth is not working try tunnels that has more luck than shotgun

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require "sinatra/config_file"
 require 'soundcloud'
 require 'hashie'
 require 'httmultiparty'
+require 'certified'
 
 
 class SoundcloudApp < Sinatra::Base
@@ -18,39 +19,28 @@ class SoundcloudApp < Sinatra::Base
     config_file "config/settings.yml"
   end
 
-  # root route (really for testing only)
-  get '/list' do
-    # flash[:notice] = "testing flash"
-    #haml :root
+  get '/' do
+    "route created"
   end
 
+
   # initializer route
-  get '/:client_id/:client_secret/:username/:password' do
+  get 'https://soundcloudapp.dev/' do
     # flash[:notice] = "testing flash"
 
-    session['client_id'] = params[:client_id];
-    session['client_secret'] = params[:client_secret];
-    session['username'] = params[:username];
-    session['password'] = params[:password];
+    #session['client_id'] = params[:client_id];
+    #session['client_secret'] = params[:client_secret];
 
-
-    # register a new client, which will exchange the username, password for an access_token
-    # NOTE: the SoundCloud API Docs advise not to use the user credentials flow in a web app.
-    # In any case, never store the password of a user.
     client = SoundCloud.new({
       :client_id     => '67925ad867fdc95e902b15afef1a6c81',
       :client_secret => '17927f3ba98bb0eec77ae19c61ded8f9',
       :username      => 'devops@gooddonegreat.com',
       :password      => 'opensaysm3'
     })
+
     # print logged in username
-    puts client.get('/me').username
-  end
+    puts ("#{client.get('/me').username}")
 
-  # View photos attached to application (main view)
-  ############################################################
-  get '/list' do
-    "route created"
+    #redirect '/list/1'
   end
-
 end
