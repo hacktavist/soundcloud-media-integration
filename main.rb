55543df8b3682403ea467969268f144cb2e1fe16
hacktavist <hacktavist616@gmail.com>
2014-04-15 15:07:19 -0400

    really fixed it this time

--- main.rb
+++ main.rb
@@ -39,6 +39,11 @@ class SoundcloudApp < Sinatra::Base
    end
 
    get '/welcome' do
+     client = SoundCloud.new({
+       :client_id => session['cid'],
+       :client_secret => session['cs'],
+       :access_token => session['at']
+       })
      name = client.get('/me').full_name;
      erb :welcome, :locals => {:username => name}
    end
